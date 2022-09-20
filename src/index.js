771a560d386613948b8bd159e38879c50d5c5d94
aidan <aid.hayes01@gmail.com>
2022-09-20 00:24:07 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import 'bootstrap/dist/css/bootstrap.css';
+class MyFirstComponent extends React.Component{
+    render(){
+        
+    }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MyFirstComponent />)
