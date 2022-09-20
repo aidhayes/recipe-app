b30940739e932cf84b0cd86f9da2851c3dab071f
aidan <aid.hayes01@gmail.com>
2022-09-20 19:02:29 -0400

    Render sidebar

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
-import Navigator from './scripts/navbar.js';
-import ProfileDropdown from './scripts/dropdown';
-import Container from 'react-bootstrap/Container';
-
+//import Navigator from './scripts/navbar.js';
+//import ProfileDropdown from './scripts/dropdown';
+//import Container from 'react-bootstrap/Container';
+//import Image from 'react-bootstrap/Image';
+import Filters from './scripts/sidebar.js';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <>
-        <Container className="container">
-            <h4>Recipe App</h4>
-            <Navigator/>
-            <ProfileDropdown/>
-        </Container>
-        
+        <h1>Cooking Daddy</h1>
+        <Filters/>
     </>
 );
