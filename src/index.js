ec66218d899ea79a0ed9bfaaa310637b695f6794
aidan <aid.hayes01@gmail.com>
2022-09-20 13:45:33 -0400

    Move stylesheet to new subfolder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css'
+import './styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
