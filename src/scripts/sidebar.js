b1fa6f7989973c0bece2964e3d22dda3ce7bfcce
Jlabianca01 <jlabianca@albany.edu>
2022-09-22 16:54:01 -0400

    edit sidebar

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -10,35 +10,74 @@ import { Dropdown } from 'react-bootstrap';
  */
 function Filters() {
     return (
-        <SidebarMenu className="w-25 mx-3 sidebar">
+        <SidebarMenu className="w-25 mx-4 sidebar">
             <SidebarMenu.Header>
-                <p className="mx-3 my-3">Select which ingredients you have, then press search to get recipes you can make!</p>
+                <p className="mx-4 my-4">Select which ingredients you have, then press search to get recipes you can make!</p>
             </SidebarMenu.Header>
             <SidebarMenu.Body>
-                <Form className="mx-3">
+                <Form className="mx-4">
                     <Row>
-                        <Dropdown>
-                                <Form.Group controlId="protein">
-                                <Dropdown.Toggle id="dropdown-basic"><Form.Label>Protein        </Form.Label></Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item><Form.Check type="checkbox" label="Chicken"/></Dropdown.Item>
-                                        <Dropdown.Item><Form.Check type="checkbox" label="Beef"/></Dropdown.Item>
-                                       <Dropdown.Item><Form.Check type="checkbox" label="Pork"/></Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Form.Group>
-                        </Dropdown>
+                        <Form.Group controlId="potein">
+                            <Form.Label>Protein</Form.Label>
+                                    
+                                <Form.Check type="checkbox" label="Chicken"/>
+                                <Form.Check type="checkbox" label="Beef"/>
+                                <Form.Check type="checkbox" label="Pork"/>
+                                <Form.Check type="checkbox" label="Egg"/>
+                                <Form.Check type="checkbox" label="Veal"/>
+                                    
+                            </Form.Group>
                     </Row>
                     <Row>
-                    <Dropdown>
                         <Form.Group controlId="vegetables">
-                            <Dropdown.Toggle id="dropdown-basic"><Form.Label>Vegetables</Form.Label></Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item><Form.Check type="checkbox" label="Carrots"/></Dropdown.Item>
-                                    <Dropdown.Item><Form.Check type="checkbox" label="Peppers"/></Dropdown.Item>
-                                    <Dropdown.Item><Form.Check type="checkbox" label="Onions"/></Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Form.Group>
-                        </Dropdown>
+                            <Form.Label>Vegetables</Form.Label>
+                                <Form.Check type="checkbox" label="Carrots"/>
+                                <Form.Check type="checkbox" label="Red Pepper"/>
+                                <Form.Check type="checkbox" label="Green Pepper"/>
+                                <Form.Check type="checkbox" label="Yellow Pepper"/>
+                                <Form.Check type="checkbox" label="Red Onions"/>
+                                <Form.Check type="checkbox" label="Yellow Onions"/>
+                                <Form.Check type="checkbox" label="Green Onions"/>
+                                <Form.Check type="checkbox" label="Tomatoes"/>
+                                <Form.Check type="checkbox" label="Broccoli"/>
+                                <Form.Check type="checkbox" label="Cucumbers"/>
+                                <Form.Check type="checkbox" label="Salad Mix"/>
+                                <Form.Check type="checkbox" label="Potatos"/>
+                                <Form.Check type="checkbox" label="Kale"/>
+                        </Form.Group>
+                    </Row>
+                    <Row>
+                        <Form.Group controlId='spices'>
+                            <Form.Label>Spices</Form.Label>
+                                <Form.Check type="checkbox" label="Cinnamon"/>
+                                <Form.Check type="checkbox" label="Ginger"/>
+                                <Form.Check type="checkbox" label="Black Pepper"/>
+                                <Form.Check type="checkbox" label="Salt"/>
+                                <Form.Check type="checkbox" label="Paprika"/>
+                                <Form.Check type="checkbox" label="Cardamom"/>
+                                <Form.Check type="checkbox" label="Turmeric Powder"/>
+                                <Form.Check type="checkbox" label="Cayenne Pepper"/>
+                                <Form.Check type="checkbox" label="Oregano"/>
+                                <Form.Check type="checkbox" label="Rosemary"/>
+                                <Form.Check type="checkbox" label="Chili Powder"/>
+                                <Form.Check type="checkbox" label="Red Pepper Flacks"/>
+                                <Form.Check type="checkbox" label="Basil"/>
+                                <Form.Check type="checkbox" label="Bay leaves"/>
+                                <Form.Check type="checkbox" label="Garlic"/>
+                                <Form.Check type="checkbox" label="Garlic Powder"/>
+                                <Form.Check type="checkbox" label="Thyme"/>
+                        </Form.Group>
+                    </Row>
+                    <Row>
+                        <Form.Group controlId='seafood'>
+                            <Form.Label>Sea Food</Form.Label>
+                                <Form.Check type="checkbox" label="Shrimp"/>
+                                <Form.Check type="checkbox" label="Tuna"/>
+                                <Form.Check type="checkbox" label="Salmon"/>
+                                <Form.Check type="checkbox" label="Tilapia"/>
+                                <Form.Check type="checkbox" label="Crab"/>
+                                <Form.Check type="checkbox" label="Shrimp"/>
+                        </Form.Group> 
                     </Row>
                 </Form>
             </SidebarMenu.Body>
