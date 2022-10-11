47195195b962b58769aee7bb8ba6b4a1a919b208
aidhayes <aid.hayes01@gmail.com>
2022-10-11 11:58:58 -0400

    Edit navbar to include site name and logo

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,25 +1,27 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/nav';
 import Navbar from 'react-bootstrap/Navbar';
 import React from 'react';
 import '../index.css';
 
 /**
  * Creates a navigation bar that will have links to the different
- * pages on the website
- * -Home
- * -Cookbook
- * -Social
  * @returns Navbar for accessing different pages on site
  */
 function Navigator() {
     return (
-        <Container className="justify-content-center">
+        <Container className="justify-content-left navbar">
             <Navbar className="navbar">
                 <Container>
-                    <Nav.Link className="links" href="#">Home</Nav.Link>
-                    <Nav.Link className="links" href="#">Cookbook</Nav.Link>
-                    <Nav.Link className="links" href="#">Social</Nav.Link>
+                    <Navbar.Brand className="brand">
+                        <img
+                            alt="Cooking Daddy Logo"
+                            src={require("../logo.png")}
+                            width="50"
+                            height="50"
+                            className="d-inline-block align-top"
+                            />{' '}
+                            Cooking Daddy
+                    </Navbar.Brand>
                 </Container>
             </Navbar>
         </Container>
