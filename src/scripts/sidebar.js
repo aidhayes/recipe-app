9b45d3eb387e28c874f6bea0e338bb3c47ac2587
aidhayes <aid.hayes01@gmail.com>
2022-10-12 17:51:48 -0400

    Create accordion to hold ingredient filters so a user will not have to scroll down everytime they want to search

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -6,6 +6,10 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import React from 'react';
 import Image from 'react-bootstrap/Image'
+import Collapse from 'react-bootstrap/Collapse';
+import Accordion from 'react-bootstrap/Accordion';
+import { useState } from 'react';
+import { AccordionButton } from 'react-bootstrap';
 
 function GetRecipes(ingredients) {
     // From https://rapidapi.com/edamam/api/recipe-search-and-diet
@@ -67,75 +71,95 @@ export default function Filters() {
         console.log(ingredients)
         GetRecipes(ingredients)
     }
+
     return (
         <>
+        
         <SidebarMenu className="mx-4 sidebar">
             <SidebarMenu.Header>
                 <p className="mx-4 my-4 pt-3">Select which ingredients you have, then press search to get recipes you can make!</p>
             </SidebarMenu.Header>
             <SidebarMenu.Body>
                 <Form className="mx-4 filters" onSubmit={onFormSubmit}>
-                    <Row>
-                        <Form.Group controlId="protein">
-                            <Form.Label>Protein</Form.Label>
-                                <Form.Check type="checkbox" name="Chicken" label="Chicken"/>
-                                <Form.Check type="checkbox" name="Beef" label="Beef"/>
-                                <Form.Check type="checkbox" name="Pork" label="Pork"/>
-                                <Form.Check type="checkbox" name="Egg" label="Egg"/>
-                                <Form.Check type="checkbox" name="Veal" label="Veal"/>    
-                            </Form.Group>
-                    </Row>
-                    <Row>
-                        <Form.Group controlId="vegetables">
-                            <Form.Label>Vegetables</Form.Label>
-                                <Form.Check type="checkbox" name="Carrots" label="Carrots"/>
-                                <Form.Check type="checkbox" name="Red Pepper" label="Red Pepper"/>
-                                <Form.Check type="checkbox" name="Green Pepper" label="Green Pepper"/>
-                                <Form.Check type="checkbox" name="Yellow Pepper" label="Yellow Pepper"/>
-                                <Form.Check type="checkbox" name="Red Onions" label="Red Onions"/>
-                                <Form.Check type="checkbox" name="Yellow Onions" label="Yellow Onions"/>
-                                <Form.Check type="checkbox" name="Green Onions" label="Green Onions"/>
-                                <Form.Check type="checkbox" name="Tomatoes" label="Tomatoes"/>
-                                <Form.Check type="checkbox" name="Broccoli" label="Broccoli"/>
-                                <Form.Check type="checkbox" name="Cucumbers" label="Cucumbers"/>
-                                <Form.Check type="checkbox" name="Salad Mix" label="Salad Mix"/>
-                                <Form.Check type="checkbox" name="Potatos" label="Potatos"/>
-                                <Form.Check type="checkbox" name="Kale" label="Kale"/>
-                        </Form.Group>
-                    </Row>
-                    <Row>
-                        <Form.Group controlId='spices'>
-                            <Form.Label>Spices</Form.Label>
-                                <Form.Check type="checkbox" name="Cannamon" label="Cinnamon"/>
-                                <Form.Check type="checkbox" name="Ginger" label="Ginger"/>
-                                <Form.Check type="checkbox" name="Black Pepper" label="Black Pepper"/>
-                                <Form.Check type="checkbox" name="Salt" label="Salt"/>
-                                <Form.Check type="checkbox" name="Paprika" label="Paprika"/>
-                                <Form.Check type="checkbox" name="Cardamon" label="Cardamom"/>
-                                <Form.Check type="checkbox" name="Turmeric Powder" label="Turmeric Powder"/>
-                                <Form.Check type="checkbox" name="Cayenne Pepper" label="Cayenne Pepper"/>
-                                <Form.Check type="checkbox" name="Oregano" label="Oregano"/>
-                                <Form.Check type="checkbox" name="Rosemary" label="Rosemary"/>
-                                <Form.Check type="checkbox" name="Chili Powder" label="Chili Powder"/>
-                                <Form.Check type="checkbox" name="Red Pepper Flakes" label="Red Pepper Flakes"/>
-                                <Form.Check type="checkbox" name="Basil" label="Basil"/>
-                                <Form.Check type="checkbox" name="Bay Leaves" label="Bay Leaves"/>
-                                <Form.Check type="checkbox" name="Garlic" label="Garlic"/>
-                                <Form.Check type="checkbox" name="Garlic Powder" label="Garlic Powder"/>
-                                <Form.Check type="checkbox" name="Thyme" label="Thyme"/>
-                        </Form.Group>
-                    </Row>
-                    <Row>
-                        <Form.Group controlId='seafood'>
-                            <Form.Label>Sea Food</Form.Label>
-                                <Form.Check type="checkbox" name="Shrimp" label="Shrimp"/>
-                                <Form.Check type="checkbox" name="Tuna" label="Tuna"/>
-                                <Form.Check type="checkbox" name="Salmon" label="Salmon"/>
-                                <Form.Check type="checkbox" name="Tilapia" label="Tilapia"/>
-                                <Form.Check type="checkbox" name="Crab" label="Crab"/>
-                                <Form.Check type="checkbox" name="Shrimp" label="Shrimp"/>
-                        </Form.Group> 
-                    </Row>
+                    <Accordion>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Protein</Accordion.Header>
+                            <Accordion.Body>
+                                <Row>
+                                <Form.Group controlId="protein">
+                                        <Form.Check type="checkbox" name="Chicken" label="Chicken"/>
+                                        <Form.Check type="checkbox" name="Beef" label="Beef"/>
+                                        <Form.Check type="checkbox" name="Pork" label="Pork"/>
+                                        <Form.Check type="checkbox" name="Egg" label="Egg"/>
+                                        <Form.Check type="checkbox" name="Veal" label="Veal"/>    
+                                </Form.Group>
+                                </Row>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Sea Food</Accordion.Header>
+                            <Accordion.Body>
+                                <Row>
+                                    <Form.Group controlId='seafood'>
+                                        <Form.Check type="checkbox" name="Shrimp" label="Shrimp"/>
+                                        <Form.Check type="checkbox" name="Tuna" label="Tuna"/>
+                                        <Form.Check type="checkbox" name="Salmon" label="Salmon"/>
+                                        <Form.Check type="checkbox" name="Tilapia" label="Tilapia"/>
+                                        <Form.Check type="checkbox" name="Crab" label="Crab"/>
+                                    </Form.Group>
+                                </Row>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>Vegetables</Accordion.Header>
+                            <Accordion.Body>
+                                <Row>
+                                    <Form.Group controlId="vegetables">
+                                                <Form.Check type="checkbox" name="Carrots" label="Carrots"/>
+                                                <Form.Check type="checkbox" name="Red Pepper" label="Red Pepper"/>
+                                                <Form.Check type="checkbox" name="Green Pepper" label="Green Pepper"/>
+                                                <Form.Check type="checkbox" name="Yellow Pepper" label="Yellow Pepper"/>
+                                                <Form.Check type="checkbox" name="Red Onions" label="Red Onions"/>
+                                                <Form.Check type="checkbox" name="Yellow Onions" label="Yellow Onions"/>
+                                                <Form.Check type="checkbox" name="Green Onions" label="Green Onions"/>
+                                                <Form.Check type="checkbox" name="Tomatoes" label="Tomatoes"/>
+                                                <Form.Check type="checkbox" name="Broccoli" label="Broccoli"/>
+                                                <Form.Check type="checkbox" name="Cucumbers" label="Cucumbers"/>
+                                                <Form.Check type="checkbox" name="Salad Mix" label="Salad Mix"/>
+                                                <Form.Check type="checkbox" name="Potatos" label="Potatos"/>
+                                                <Form.Check type="checkbox" name="Kale" label="Kale"/>
+                                    </Form.Group>
+                                </Row>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="3">
+                            <Accordion.Header>Spices</Accordion.Header>
+                            <Accordion.Body>
+                                <Row>
+                                    <Form.Group controlId='spices'>
+                                    <Form.Label>Spices</Form.Label>
+                                        <Form.Check type="checkbox" name="Cannamon" label="Cinnamon"/>
+                                        <Form.Check type="checkbox" name="Ginger" label="Ginger"/>
+                                        <Form.Check type="checkbox" name="Black Pepper" label="Black Pepper"/>
+                                        <Form.Check type="checkbox" name="Salt" label="Salt"/>
+                                        <Form.Check type="checkbox" name="Paprika" label="Paprika"/>
+                                        <Form.Check type="checkbox" name="Cardamon" label="Cardamom"/>
+                                        <Form.Check type="checkbox" name="Turmeric Powder" label="Turmeric Powder"/>
+                                        <Form.Check type="checkbox" name="Cayenne Pepper" label="Cayenne Pepper"/>
+                                        <Form.Check type="checkbox" name="Oregano" label="Oregano"/>
+                                        <Form.Check type="checkbox" name="Rosemary" label="Rosemary"/>
+                                        <Form.Check type="checkbox" name="Chili Powder" label="Chili Powder"/>
+                                        <Form.Check type="checkbox" name="Red Pepper Flakes" label="Red Pepper Flakes"/>
+                                        <Form.Check type="checkbox" name="Basil" label="Basil"/>
+                                        <Form.Check type="checkbox" name="Bay Leaves" label="Bay Leaves"/>
+                                        <Form.Check type="checkbox" name="Garlic" label="Garlic"/>
+                                        <Form.Check type="checkbox" name="Garlic Powder" label="Garlic Powder"/>
+                                        <Form.Check type="checkbox" name="Thyme" label="Thyme"/>
+                                    </Form.Group>
+                                </Row>      
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
                     <Button variant="primary" type="submit">Submit</Button>
                 </Form>
             </SidebarMenu.Body>
