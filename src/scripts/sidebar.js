b14ed3b3dcbaa00f258f7366500515893b583b5e
Ashton Harris <achar96@DESKTOP-A691014.localdomain>
2022-10-17 23:29:57 -0400

    Major refactor to how recipe hits are recieved and stored, added favorite button to UI with images

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -9,43 +9,16 @@ import Image from 'react-bootstrap/Image'
 import Accordion from 'react-bootstrap/Accordion';
 import { useState } from 'react';
 
+   
+
 /**
  * Creates sidebar with filters to search for recipes
  * @returns Sidebar for filtering recipes
  */
 export default function Filters() {
+    const [recipeArray, setRecipeArray] = useState(["","","","","","","","","",""]);
 
-    const [imgUrl, setImgUrl] = useState("");
-	const [imgUrl1, setImgUrl1] = useState("");
-	const [imgUrl2, setImgUrl2] = useState("");
-	const [imgUrl3, setImgUrl3] = useState("");
-	const [imgUrl4, setImgUrl4] = useState("");
-	const [imgUrl5, setImgUrl5] = useState("");
-	const [imgUrl6, setImgUrl6] = useState("");
-	const [imgUrl7, setImgUrl7] = useState("");
-	const [imgUrl8, setImgUrl8] = useState("");
-	const [imgUrl9, setImgUrl9] = useState("");
-	const [imgtxt, setImgTxt] = useState("")
-	const [imgtxt1, setImgTxt1] = useState("")
-	const [imgtxt2, setImgTxt2] = useState("")
-	const [imgtxt3, setImgTxt3] = useState("")
-	const [imgtxt4, setImgTxt4] = useState("")
-	const [imgtxt5, setImgTxt5] = useState("")
-	const [imgtxt6, setImgTxt6] = useState("")
-	const [imgtxt7, setImgTxt7] = useState("")
-	const [imgtxt8, setImgTxt8] = useState("")
-	const [imgtxt9, setImgTxt9] = useState("")
-	const [imgLink, setImgLink] = useState("")
-	const [imgLink1, setImgLink1] = useState("")
-	const [imgLink2, setImgLink2] = useState("")
-	const [imgLink3, setImgLink3] = useState("")
-	const [imgLink4, setImgLink4] = useState("")
-	const [imgLink5, setImgLink5] = useState("")
-	const [imgLink6, setImgLink6] = useState("")
-	const [imgLink7, setImgLink7] = useState("")
-	const [imgLink8, setImgLink8] = useState("")
-	const [imgLink9, setImgLink9] = useState("")
-	
+
     /**
      * https://stackoverflow.com/questions/63182107/how-can-i-get-a-value-from-a-react-bootstrap-form-on-submit
      * @param {*} e 
@@ -75,69 +48,23 @@ export default function Filters() {
 
         axios.request(options).then(function (response) {
             console.log(response.data);
-            //var url = response.data.hits[1].recipe.url;
-            var imgurl = response.data.hits[0].recipe.image;
-			var imgtxt = response.data.hits[0].recipe.label;
-			var imgLink = response.data.hits[0].recipe.url;
-			var imgurl1 = response.data.hits[1].recipe.image;
-			var imgtxt1 = response.data.hits[1].recipe.label;
-			var imgLink1 = response.data.hits[1].recipe.url;
-			var imgurl2 = response.data.hits[2].recipe.image;
-			var imgtxt2 = response.data.hits[2].recipe.label;
-			var imgLink2 = response.data.hits[2].recipe.url;
-			var imgurl3 = response.data.hits[3].recipe.image;
-			var imgtxt3 = response.data.hits[3].recipe.label;
-			var imgLink3 = response.data.hits[3].recipe.url;
-			var imgurl4 = response.data.hits[4].recipe.image;
-			var imgtxt4 = response.data.hits[4].recipe.label;
-			var imgLink4 = response.data.hits[4].recipe.url;
-			var imgurl5 = response.data.hits[5].recipe.image;
-			var imgtxt5 = response.data.hits[5].recipe.label;
-			var imgLink5 = response.data.hits[5].recipe.url;
-			var imgurl6 = response.data.hits[6].recipe.image;
-			var imgtxt6 = response.data.hits[6].recipe.label;
-			var imgLink6 = response.data.hits[6].recipe.url;
-			var imgurl7 = response.data.hits[7].recipe.image;
-			var imgtxt7 = response.data.hits[7].recipe.label;
-			var imgLink7 = response.data.hits[7].recipe.url;
-			var imgurl8 = response.data.hits[8].recipe.image;
-			var imgtxt8 = response.data.hits[8].recipe.label;
-			var imgLink8 = response.data.hits[8].recipe.url;
-			var imgurl9 = response.data.hits[9].recipe.image;
-			var imgtxt9 = response.data.hits[9].recipe.label;
-			var imgLink9 = response.data.hits[9].recipe.url;
-            //console.log(imgurl);
-            setImgUrl(imgurl);
-			setImgTxt(imgtxt);
-			setImgLink(imgLink);
-			setImgUrl1(imgurl1);
-			setImgTxt1(imgtxt1);
-			setImgLink1(imgLink1);
-			setImgUrl2(imgurl2);
-			setImgTxt2(imgtxt2);
-			setImgLink2(imgLink2);
-			setImgUrl3(imgurl3);
-			setImgTxt3(imgtxt3);
-			setImgLink3(imgLink3);
-			setImgUrl4(imgurl4);
-			setImgTxt4(imgtxt4);
-			setImgLink4(imgLink4);
-			setImgUrl5(imgurl5);
-			setImgTxt5(imgtxt5);
-			setImgLink5(imgLink5);
-			setImgUrl6(imgurl6);
-			setImgTxt6(imgtxt6);
-			setImgLink6(imgLink6);
-			setImgUrl7(imgurl7);
-			setImgTxt7(imgtxt7);
-			setImgLink7(imgLink7);
-			setImgUrl8(imgurl8);
-			setImgTxt8(imgtxt8);
-			setImgLink8(imgLink8);
-			setImgUrl9(imgurl9);
-			setImgTxt9(imgtxt9);
-			setImgLink9(imgLink9);
-            //console.log(url);
+
+            //Make temp arrays to store recipe data
+            var tempImgs = [];
+            var tempTxts = [];
+            var tempUrls = [];
+            for(let i = 0; i <10; i++){ // Fill the arrays (10 hits)
+                tempImgs[i] = response.data.hits[i].recipe.image;
+                tempTxts[i] = response.data.hits[i].recipe.label;
+                tempUrls[i] = response.data.hits[i].recipe.url;
+            }
+            var tempArray = [] // Temp recipe Array for storing images
+            for(let i = 0; i < 10; i++){ // Fill recipe array with images
+                var recipe = <a href={tempUrls[i]}><Image src={tempImgs[i]}/><Button variant="primary" type="submit">Favorite</Button><h2>{tempTxts[i]}</h2></a>;
+                tempArray.push(recipe);
+            }
+            setRecipeArray(tempArray); // Set the recipe array which is loaded on form submit
+
          }).catch(function (error) {
              console.error(error);
          });
@@ -356,49 +283,15 @@ export default function Filters() {
             </SidebarMenu.Body>
         </SidebarMenu>
         <Container>
-            <Row>
-				<a href={imgLink}>
-                  <Image src={imgUrl}/>
-				</a>
-				<h2>{imgtxt}</h2>
-				<a href={imgLink1}>
-				  <Image src={imgUrl1}/>
-				</a>
-				<h2>{imgtxt1}</h2>
-				<a href={imgLink2}>
-				  <Image src={imgUrl2}/>
-				</a>
-				<h2>{imgtxt2}</h2>
-				<a href={imgLink3}>
-				  <Image src={imgUrl3}/>
-				</a>
-				<h2>{imgtxt3}</h2>
-				<a href={imgLink4}>
-				  <Image src={imgUrl4}/>
-				</a>
-				<h2>{imgtxt4}</h2>
-				<a href={imgLink5}>
-				  <Image src={imgUrl5}/>
-				</a>
-				<h2>{imgtxt5}</h2>
-				<a href={imgLink6}>
-				  <Image src={imgUrl6}/>
-				</a>
-				<h2>{imgtxt6}</h2>
-				<a href={imgLink7}>
-				  <Image src={imgUrl7}/>
-				</a>
-				<h2>{imgtxt7}</h2>
-				<a href={imgLink8}>
-				  <Image src={imgUrl8}/>
-				</a>
-				<h2>{imgtxt8}</h2>
-				<a href={imgLink9}>
-				  <Image src={imgUrl9}/>
-				</a>
-				<h2>{imgtxt9}</h2>
-            </Row>
+
+                <tbody>                   
+                    {
+                        recipeArray // Display recipe array
+                    } 
+                </tbody>
+                                                                                                                                                                                      
         </Container>
         </>
     );
 }
+
