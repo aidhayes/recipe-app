1d27f09475782ab68936d6d6e9a11c59954dddf4
aidhayes <aid.hayes01@gmail.com>
2022-11-02 14:09:49 -0400

    Import navbar

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,12 +1,13 @@
 import Button from 'react-bootstrap/Button';
 import '../index.css';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Navigator from '../scripts/navbar';
 
 export default function Root() {
     return (
         <>
+            <Navigator/>
             <Container>
                 <Col className="welcome" id="welcome">
                     <b>Welcome to Cooking Daddy</b>, a site dedicated to helping those looking for a quick meal find recipes.
