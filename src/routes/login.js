47437e0a9b9f67807be1fa480b14e88f7ce2a131
aidhayes <aid.hayes01@gmail.com>
2022-11-05 14:39:38 -0400

    Basic login text entry

--- src/routes/login.js
+++ src/routes/login.js
@@ -7,21 +7,29 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import '../index.css';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Col from 'react-bootstrap/Col';
 
 
 export default function Login() {
     return (
         <>
             <Container className="welcome">
-                <p> Login </p>
-                <InputGroup>
-                    <Form.Control
-                        placeholder="Username/Email"
-                        aria-label="Username/Email"
-                        aria-describedby="basic-addon1"
-                    />
-                </InputGroup>
+                <Col>
+                    Login
+                
+                    <Form.Group>
+                        <Form.Control
+                            placeholder="Username/Email"
+                            aria-label="Username/Email"
+                            aria-describedby="basic-addon1"
+                        />
+                        <Form.Control
+                            placeholder="Password"
+                            aria-label="Password"
+                            aria-describedby="basic-addon1"
+                        />
+                    </Form.Group>
+                </Col>
             </Container>
         </>
     )
