bf0b59ba9e99ccdab64229e40af144381326d055
aidhayes <aid.hayes01@gmail.com>
2022-11-05 14:12:48 -0400

    Basic login page

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,14 +1,28 @@
 /** 
-import Button from 'react-bootstrap/Button';
-import '../index.css';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
+
 import { LinkContainer } from 'react-router-bootstrap';
 */
+
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import '../index.css';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+
 export default function Login() {
     return (
         <>
-
+            <Container className="welcome">
+                <p> Login </p>
+                <InputGroup>
+                    <Form.Control
+                        placeholder="Username/Email"
+                        aria-label="Username/Email"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </Container>
         </>
     )
 }
