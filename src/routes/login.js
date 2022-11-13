ea1a57c9bc16c48aaa65a6909fbe88620a0c5af7
Matthew Melendez <32375336+MatthewM6050@users.noreply.github.com>
2022-11-12 23:12:21 -0500

    Updates packages.json and login.js
    Changed "scripts" tag on package.js to run on my macbook or else it doesnt run local server.
    
    I also started implementing Firebase into the login.js. I started the fundementals such as importing fuctions and defining them.

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,11 @@
 /** 
 
 import { LinkContainer } from 'react-router-bootstrap';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 */
 
+//Imports
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import '../index.css';
@@ -11,6 +14,11 @@ import Col from 'react-bootstrap/Col';
 import InputGroup from 'react-bootstrap/InputGroup';
 import React, { useState } from 'react';
 import {FaEye} from 'react-icons/fa';
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth, 
+    connectAuthEmulator, 
+    signInWithEmailAndPassword} from 'firebase/auth';
 
 // Login page
 export default function Login() {
@@ -26,6 +34,33 @@ export default function Login() {
         setVisibility(showPassword ? false : true);
     };
 
+    //Login Authentication using Firebase
+    //Variables for Firebase
+    const firebaseApp = initializeApp({
+        apiKey: "AIzaSyD4pNnT98nR04rDvKvLWcNqrnv3zdwOqX0",
+        authDomain: "recipe-app-441d8.firebaseapp.com",
+        projectId: "recipe-app-441d8",
+        storageBucket: "recipe-app-441d8.appspot.com",
+        messagingSenderId: "86988559479",
+        appId: "1:86988559479:web:1f8a8ee3bb060674272272",
+        measurementId: "G-VQ7XEJT7QW"
+    });
+    const auth = getAuth(firebaseApp);
+    connectAuthEmulator(auth, 'http://localhost:3000');
+
+    //Functions for Firebase
+    const loginEmailPassword = async () => {
+        const loginEmail = document.getElementById('txtEmail').value;
+        const loginPassword = document.getElementById('txtPassword').value;
+
+        const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+        console.log(userCredential.user);
+    }
+
+
+
+
+
     return (
         <>
             <Container className="welcome">
@@ -34,12 +69,14 @@ export default function Login() {
                 
                     <Form.Group>
                         <Form.Control
+                            id = "txtEmail"
                             placeholder="Username/Email"
                             aria-label="Username/Email"
                             aria-describedby="basic-addon1"
                         />
                         <InputGroup>
                             <Form.Control
+                                id = "txtPassword"
                                 placeholder="Password"
                                 type={showPassword ? "text": "password"}
                                 aria-label="Password"
@@ -50,7 +87,9 @@ export default function Login() {
                             </Button>
                             
                         </InputGroup>
-                        <Button variant="shadow-non" type="submit" className="submit">Login</Button>
+                        <Button variant="shadow-non" type="submit" className="submit">Login
+                        <i onClick={loginEmailPassword}></i>{''}
+                        </Button>
                     </Form.Group>
                 </Col>
             </Container>
