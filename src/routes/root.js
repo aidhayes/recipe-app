18c7a92c6e6975cb3d3503c582f9eb6150b5f08a
aidhayes <aid.hayes01@gmail.com>
2022-11-15 20:15:07 -0500

    Import react

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,6 +3,7 @@ import '../index.css';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import { LinkContainer } from 'react-router-bootstrap';
+import React from 'react';
 
 // Landing page
 export default function Root() {
--- src/routes/search.js
+++ src/routes/search.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import '../index.css';
 
 import Filters from '../scripts/sidebar';
