f159f8877897069e6a3fe9e603341f79fe58f6b8
aidhayes <aid.hayes01@gmail.com>
2022-12-02 13:19:10 -0500

    Comment out unneeded import

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+//import Col from 'react-bootstrap/Col';
 export default function Favorites() {
     let recipes = [];
     let favorites = JSON.parse(window.localStorage.getItem("favorites"));
